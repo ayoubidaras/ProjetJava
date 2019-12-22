e30d5c0520c2451dc9134eb261b5d1239ebabcab
BBnours <olivier.paulo13@gmail.com>
2019-12-22 20:09:48 +0100

    essaie de réalisation du login 🙃�

---
+++ Java/src/Vue/LoginFrame.java
@@ -0,0 +1,23 @@
+package Vue;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class LoginFrame extends JFrame{
+    private JPanel LoginPanel;
+    private JButton btn_connexion;
+
+    public LoginFrame() {
+        btn_connexion.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+
+            }
+        });
+    }
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
