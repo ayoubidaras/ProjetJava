900028dd397f61fc25e2a69332d8842a365c5651
BBnours <olivier.paulo13@gmail.com>
2019-12-22 20:39:56 +0100

    toujours en phase d'essaie de login

--- Java/src/Vue/LoginFrame.java
+++ Java/src/Vue/LoginFrame.java
@@ -4,7 +4,7 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class LoginFrame extends JFrame{
+public class LoginFrame {
     private JPanel LoginPanel;
     private JButton btn_connexion;
 
@@ -12,12 +12,16 @@ public class LoginFrame extends JFrame{
         btn_connexion.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
-
+                JOptionPane.showMessageDialog(null,"Hello World !");
             }
         });
     }
 
-    private void createUIComponents() {
-        // TODO: place custom component creation code here
+    public static void main(String[] args){
+         JFrame frame = new JFrame("LoginFrame");
+         frame.setContentPane(new App().panelMain);
+         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+         frame.pack();
+         frame.setVisible(true);
     }
 }
